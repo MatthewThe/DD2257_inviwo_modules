5083dabea5cbdc1b8362e1358adc950107c8c3ef
Avani Yu <avaniyu@users.noreply.github.com>
2017-09-11 19:00:10 +0200

    Update generate2ddata.cpp

--- dd2257lab1/generate2ddata.cpp
+++ dd2257lab1/generate2ddata.cpp
@@ -1,5 +1,5 @@
 /*********************************************************************
- *  Author  : Wiebke Koepp, ... 
+ *  Author  : Wiebke Koepp, Jiayao Yu 
  *  Init    : Friday, September 01, 2017 - 12:13:44
  *
  *  Project : KTH Inviwo Modules
@@ -9,95 +9,118 @@
  */
 
 #include <dd2257lab1/generate2ddata.h>
+#include <math.h>
+
+#define PI 3.14159265
 
 namespace inviwo
 {
 
-// The Class Identifier has to be globally unique. Use a reverse DNS naming scheme
-const ProcessorInfo Generate2DData::processorInfo_
-{
-    "org.inviwo.Generate2DData",      // Class identifier
-    "Generate 2D Data",               // Display name
-    "DD2257",                         // Category
-    CodeState::Experimental,          // Code state
-    "CPU, DataFrame"                  // Tags
-};
+	// The Class Identifier has to be globally unique. Use a reverse DNS naming scheme
+	const ProcessorInfo Generate2DData::processorInfo_
+	{
+		"org.inviwo.Generate2DData",      // Class identifier
+		"Generate 2D Data",               // Display name
+		"DD2257",                         // Category
+		CodeState::Experimental,          // Code state
+		"CPU, DataFrame"                  // Tags
+	};
 
-const ProcessorInfo Generate2DData::getProcessorInfo() const
-{
-    return processorInfo_;
-}
+	const ProcessorInfo Generate2DData::getProcessorInfo() const
+	{
+		return processorInfo_;
+	}
 
-Generate2DData::Generate2DData()
-    :Processor()
-    , outDataFrame("outDataFrame")
-    , propdataShape("dataShape", "Data Shape")
-{
-    //Register ports
-    
-    addPort(outDataFrame);
-    
-    // Register properties
+	Generate2DData::Generate2DData()
+		:Processor()
+		, outDataFrame("outDataFrame")
+		, propdataShape("dataShape", "Data Shape")
+	{
+		//Register ports
 
-    addProperty(propdataShape);
-    propdataShape.addOption("line", "Line", DataShape::Line);
-    propdataShape.addOption("circle", "Circle", DataShape::Circle);
-    propdataShape.addOption("hyperbola", "Hyperbola", DataShape::Hyperbola);
-    propdataShape.set(DataShape::Line);
-    propdataShape.setCurrentStateAsDefault();
-}
+		addPort(outDataFrame);
+
+		// Register properties
+
+		addProperty(propdataShape);
+		propdataShape.addOption("line", "Line", DataShape::Line);
+		propdataShape.addOption("circle", "Circle", DataShape::Circle);
+		propdataShape.addOption("hyperbola", "Hyperbola", DataShape::Hyperbola);
+		propdataShape.set(DataShape::Line);
+		//propdataShape.set(DataShape::Circle);
+		//propdataShape.set(DataShape::Hyperbola);
+		propdataShape.setCurrentStateAsDefault();
+	}
 
 
-void Generate2DData::process()
-{
-    // Initialize the dataframe
-    auto dataframe = std::make_shared<DataFrame>();
+	void Generate2DData::process()
+	{
+		// Initialize the dataframe
+		auto dataframe = std::make_shared<DataFrame>();
 
-    // Create data container with two columns
-    auto colX = dataframe->addColumn<float>("x");
-    auto colY = dataframe->addColumn<float>("y");
+		// Create data container with two columns
+		auto colX = dataframe->addColumn<float>("x");
+		auto colY = dataframe->addColumn<float>("y");
 
-    // Lamda function for adding a single data point
-    auto addPoint = [&](float x, float y)
-    {
-        colX->add(x);
-        colY->add(y);
-    };
+		// Lamda function for adding a single data point
+		auto addPoint = [&](float x, float y)
+		{
+			colX->add(x);
+			colY->add(y);
+		};
 
-    switch (propdataShape.get())
-    {
-        case DataShape::Line: {
+		switch (propdataShape.get())
+		{
+		case DataShape::Line: {
+			int step_line = 20;
+			float stepLengthX_line = 0.5;
+			float stepLengthY_line = 0.5;
+			// TODO: Add points that lie on a line with negative slope
+			//addPoint(0.3f, 0.4f);
+			float x_line[20] = { 0. };
+			float y_line[20] = { 20. };
+			for (int i = 0; i < step_line; i++) {
+				x_line[i] += i*stepLengthX_line;
+				y_line[i] -= i*stepLengthY_line;
+				addPoint(x_line[i], y_line[i]);
+			}
+			break;
+		}
+		case DataShape::Circle: {
+			int step_circle = 36;
+			float r = 10;
+			int x_origin, y_origin = 5;
+			float x_circle[36], y_circle[36] = { 0. };
+			// TODO: Add points that lie on a circle
+			for (int i = 0; i < step_circle; i++) {
+				x_circle[i] = x_origin + r*cos(i * (360 / step_circle) * 2 * PI / 180);
+				y_circle[i] = y_origin + r*sin(i * (360 / step_circle) * 2 * PI / 180);
+				addPoint(x_circle[i], y_circle[i]);
+			}
+			//addPoint(0.24f, 0.4f);
 
-            // TODO: Add points that lie on a line with negative slope
-            addPoint(0.3f, 0.4f);
+			break;
+		}
+		case DataShape::Hyperbola: {
 
-            break;
-        }
-        case DataShape::Circle: {
+			// TODO: Add points that lie on a hyperbola
+			addPoint(0.3f, 0.4f);
 
-            // TODO: Add points that lie on a circle
-            addPoint(0.24f, 0.4f);
-            break;
-        }
-        case DataShape::Hyperbola: {
+			break;
+		}
+		default: {
+			break;
+		}
+		}
 
-            // TODO: Add points that lie on a hyperbola
-            addPoint(0.3f, 0.4f);
+		// We did not take care of the indices (first column of the dataframe)
+		// so we will do this now
+		dataframe->updateIndexBuffer();
 
-            break;
-        }
-        default: {
-            break;
-        }
-    }
-
-    // We did not take care of the indices (first column of the dataframe)
-    // so we will do this now
-    dataframe->updateIndexBuffer();
-
-    // Push the created data out
-    outDataFrame.setData(dataframe);
-}
+		// Push the created data out
+		outDataFrame.setData(dataframe);
+	}
 
 } // namespace
+
 
