40edb705c13185250084b61150041fb44d188eed
Pontus-S <pkgstr@kth.se>
2017-10-06 15:37:00 +0200

    Assignment 6

---
+++ dd2257lab4/integrator.cpp
@@ -0,0 +1,23 @@
+/*********************************************************************
+ *  Author  : Himangshu Saikia
+ *  Init    : Wednesday, September 20, 2017 - 12:04:15
+ *
+ *  Project : KTH Inviwo Modules
+ *
+ *  License : Follows the Inviwo BSD license model
+ *********************************************************************
+ */
+
+#include <dd2257lab4/integrator.h>
+
+namespace inviwo
+{
+
+Integrator::Integrator()
+{
+}
+
+// TODO: Implementation of the functions defined in the header file integrator.h
+
+} // namespace
+
